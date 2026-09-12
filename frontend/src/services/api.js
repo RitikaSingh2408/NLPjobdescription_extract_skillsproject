e65c28c3dd657fed026083f6702955055f9588b4
Ritika <ritu@gmail.com>
2026-09-12 16:27:47 +0530

    connect render

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://nlpjobdescription-extract-skillsproject.onrender.com";
 
 export async function registerUser(userData) {
   const response = await fetch(
